70e6838997e866e042913d09d2c0910f1a7a01dd
Curious <13582788510@139.com>
2023-03-10 01:05:39 +0800

    Add files via upload

---
+++ example-custom-script-1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# 
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+#-------------------------------------------------------------------------------------------------------
+#
+#
+# This script will run before feeds update, something you want to do at that moment should be written here.
+# A common function of this script is to modify the cloned OpenWrt source code. 
+#
+# For instance, you can edit the feeds.conf.default to induct packages you need.
+# This is followed by some editing examples.
+# # Clear the feeds.conf.default and append the feed sources you need one by one:
+#cat /dev/null > a.txt
+#echo 'src-git-full packages https://git.openwrt.org/feed/packages.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full luci https://git.openwrt.org/project/luci.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-22.03' >> feeds.conf.default
+#echo 'src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03' >> feeds.conf.default
+# # Replace a feed source with what you want:
+#sed '/feeds-name/'d feeds.conf.default
+#echo 'method feed-name path/URL' >> feeds.conf.default
+# # Uncomment a feed source:
+#sed -i 's/^#\(.*feed-name\)/\1/' feeds.conf.default
+#
+# You can also modify the source code by patching.
+# # Here's a template for patching:
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
+#EOF
+#git apply example.patch
+
---
+++ example-custom-script-2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2022-2023 Curious <https://www.curious.host>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+# 
+# https://github.com/Curious-r/OpenWrtBuildWorkflows
+# Description: Automatically check OpenWrt source code update and build it. No additional keys are required.
+#-------------------------------------------------------------------------------------------------------
+#
+#
+# Patching is generally recommended.
+# # Here's a template for patching:
+#touch example.patch
+#cat>example.patch<<EOF
+#patch content
+#EOF
+#git apply example.patch
+
+
